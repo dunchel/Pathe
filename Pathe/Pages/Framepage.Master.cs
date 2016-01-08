a7751f0f63ba2c7cbb86ca23e076fd2d99aa2e88
dundy <dundy_pura_7@live.com>
2016-01-08 19:03:19 +0100

    load page
    menu master page

--- Pathe/Pages/Framepage.Master.cs
+++ Pathe/Pages/Framepage.Master.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Pathe.Database;
+using Pathe.Classes;
+using Pathe.Cache;
 
 namespace Pathe.Pages
 {
@@ -11,7 +14,22 @@ namespace Pathe.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            RefreshMenu();
 
         }
+
+
+
+        private void RefreshMenu()
+        {
+            string Truth = "true";
+            string lie = "false";
+            if (Session["IsLoggedIn"] == null)
+            {
+                Session["IsLoggedIn"] = lie;
+                
+            }
+        }
     }
+
 }
